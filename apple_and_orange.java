73e08d28607c58874e59e64db67bfd4b5b8c56b4
Sanghamitra1234 <sanghota4567@gmail.com>
2018-10-04 02:24:07 +0530

    hackerrank problems

---
+++ apple_and_orange.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the countApplesAndOranges function below.
+     */
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+      int count1=0,count2=0;
+      for(int i=0;i<apples.length;i++){
+        int a1=a+apples[i];
+        if(a1>=s&&a1<=t){
+          count1++;
+        }
+      }
+      for(int j=0;j<oranges.length;j++){
+        int a1=b+oranges[j];
+        if(a1>=s&&a1<=t){
+          count2++;
+        }
+      }
+      System.out.println(count1);
+      System.out.println(count2);
+
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scan.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0].trim());
+
+        int t = Integer.parseInt(st[1].trim());
+
+        String[] ab = scan.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0].trim());
+
+        int b = Integer.parseInt(ab[1].trim());
+
+        String[] mn = scan.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0].trim());
+
+        int n = Integer.parseInt(mn[1].trim());
+
+        int[] apple = new int[m];
+
+        String[] appleItems = scan.nextLine().split(" ");
+
+        for (int appleItr = 0; appleItr < m; appleItr++) {
+            int appleItem = Integer.parseInt(appleItems[appleItr].trim());
+            apple[appleItr] = appleItem;
+        }
+
+        int[] orange = new int[n];
+
+        String[] orangeItems = scan.nextLine().split(" ");
+
+        for (int orangeItr = 0; orangeItr < n; orangeItr++) {
+            int orangeItem = Integer.parseInt(orangeItems[orangeItr].trim());
+            orange[orangeItr] = orangeItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apple, orange);
+    }
+}
