af20bafeaebef0de9e10fdbce814b7932ef083ce
Anshu Patel <patelanshu85@gmail.com>
2018-10-22 21:38:17 +0530

    DivisibleSumPairs

---
+++ DivisibleSumPairs.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int divisibleSumPairs(int n, int k, vector <int> ar) {
+    // Complete this function
+    int count=0;
+    for(int j=0;j<n;j++){
+        for(int i=0;i<j;i++){
+            if((ar[i]+ar[j])%k==0)
+                count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> ar(n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       cin >> ar[ar_i];
+    }
+    int result = divisibleSumPairs(n, k, ar);
+    cout << result << endl;
+    return 0;
+}
